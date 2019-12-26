f1bc75a613f760f858ed5e611a1665a56c9ba8d4
Liu Yunzhi <liuyunzhi95@gmail.com>
2019-12-26 13:09:54 +0800

    feature(game): implement many throws

--- src/main/java/bowling/Game.java
+++ src/main/java/bowling/Game.java
@@ -2,12 +2,23 @@ package bowling;
 
 public class Game {
     private int itsScore = 0;
+    private int[] itsThrows = new int[21];
+    private int itsCurrentThrow = 0;
 
     public void add(final int pins) {
+        itsThrows[itsCurrentThrow++] = pins;
         itsScore += pins;
     }
 
     public int score() {
         return itsScore;
+    }
+
+    public int scoreForFrame(int frame) {
+        int score = 0;
+        for (int ball = 0; frame > 0 && (ball < itsCurrentThrow); ball += 2, frame--) {
+            score += itsThrows[ball] + itsThrows[ball + 1];
+        }
+        return score;
     }
 }
--- src/test/java/bowling/GameTest.java
+++ src/test/java/bowling/GameTest.java
@@ -14,4 +14,28 @@ public class GameTest {
 
         assertEquals(5, game.score());
     }
+
+    @Test
+    void two_throws_no_mark() {
+        final Game game = new Game();
+
+        game.add(5);
+        game.add(4);
+
+        assertEquals(9, game.score());
+    }
+
+    @Test
+    void four_throws_no_mark() {
+        final Game game = new Game();
+
+        game.add(5);
+        game.add(4);
+        game.add(7);
+        game.add(2);
+
+        assertEquals(18, game.score());
+        assertEquals(9, game.scoreForFrame(1));
+        assertEquals(18, game.scoreForFrame(2));
+    }
 }
