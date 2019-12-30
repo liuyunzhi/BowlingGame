bee6ad2ade6111974664076934b871979544bb1f
Liu Yunzhi <liuyunzhi95@gmail.com>
2019-12-30 23:09:45 +0800

    refactor(game): extract method adjustFrameForStrike

--- src/main/java/bowling/Game.java
+++ src/main/java/bowling/Game.java
@@ -24,15 +24,21 @@ public class Game {
 
     private void adjustCurrentFrame(int pins) {
         if (firstThrowInFrame) {
-            if (pins == 10) {
-                advanceFrame();
-            } else {
+            if (!adjustFrameForStrike(pins)) {
                 firstThrowInFrame = false;
             }
         } else {
             firstThrowInFrame = true;
             advanceFrame();
         }
+    }
+
+    private boolean adjustFrameForStrike(int pins) {
+        if (pins == 10) {
+            advanceFrame();
+            return true;
+        }
+        return false;
     }
 
     private void advanceFrame() {
