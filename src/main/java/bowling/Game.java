330886241679de63b126d8e893e4b48111471960
Liu Yunzhi <liuyunzhi95@gmail.com>
2019-12-30 19:56:40 +0800

    refactor(game): extract field

--- src/main/java/bowling/Game.java
+++ src/main/java/bowling/Game.java
@@ -7,6 +7,10 @@ public class Game {
     private int itsCurrentFrame = 1;
     private boolean firstThrowInFrame = true;
 
+    private int ball;
+    private int firstThrow;
+    private int secondThrow;
+
     public void add(final int pins) {
         itsThrows[itsCurrentThrow++] = pins;
         itsScore += pins;
@@ -32,14 +36,14 @@ public class Game {
     }
 
     public int scoreForFrame(int theFrame) {
-        int ball = 0;
+        ball = 0;
         int score = 0;
         for (int currentFrame = 0; currentFrame < theFrame; currentFrame++) {
-            int firstThrow = itsThrows[ball++];
+            firstThrow = itsThrows[ball++];
             if (firstThrow == 10) {
                 score += 10 + itsThrows[ball] + itsThrows[ball + 1];
             } else {
-                int secondThrow = itsThrows[ball++];
+                secondThrow = itsThrows[ball++];
                 int frameScore = firstThrow + secondThrow;
                 if (frameScore == 10) {
                     score += frameScore + itsThrows[ball];
