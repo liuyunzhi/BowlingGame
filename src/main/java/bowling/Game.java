2b570f3f868127f02e8bac89ae3ae11a7e688490
Liu Yunzhi <liuyunzhi95@gmail.com>
2019-12-30 22:58:11 +0800

    refactor(game): extract delegate class Scorer

--- src/main/java/bowling/Game.java
+++ src/main/java/bowling/Game.java
@@ -2,21 +2,14 @@ package bowling;
 
 public class Game {
     private int itsScore = 0;
-    private int[] itsThrows = new int[21];
-    private int itsCurrentThrow = 0;
     private int itsCurrentFrame = 1;
     private boolean firstThrowInFrame = true;
-
-    private int ball;
+    private Scorer itsScorer = new Scorer();
 
     public void add(final int pins) {
-        addThrow(pins);
+        itsScorer.addThrow(pins);
         itsScore += pins;
         adjustCurrentFrame(pins);
-    }
-
-    private void addThrow(final int pins) {
-        itsThrows[itsCurrentThrow++] = pins;
     }
 
     public int score() {
@@ -28,21 +21,7 @@ public class Game {
     }
 
     public int scoreForFrame(int theFrame) {
-        ball = 0;
-        int score = 0;
-        for (int currentFrame = 0; currentFrame < theFrame; currentFrame++) {
-            if (strike()) {
-                score += 10 + nextTwoBallsForStrike();
-                ball++;
-            } else if (spare()) {
-                score += 10 + nextBallForSpare();
-                ball += 2;
-            } else {
-                score += twoBallsInFrame();
-                ball += 2;
-            }
-        }
-        return score;
+        return itsScorer.scoreForFrame(theFrame);
     }
 
     private void adjustCurrentFrame(int pins) {
@@ -57,25 +36,5 @@ public class Game {
             itsCurrentFrame++;
         }
         itsCurrentFrame = Math.min(11, itsCurrentFrame);
-    }
-
-    private boolean strike() {
-        return itsThrows[ball] == 10;
-    }
-
-    private int nextTwoBallsForStrike() {
-        return itsThrows[ball + 1] + itsThrows[ball + 2];
-    }
-
-    private int twoBallsInFrame() {
-        return itsThrows[ball] + itsThrows[ball + 1];
-    }
-
-    private boolean spare() {
-        return itsThrows[ball] + itsThrows[ball + 1] == 10;
-    }
-
-    private int nextBallForSpare() {
-        return itsThrows[ball + 2];
     }
 }
